7e4e20683575d61207728375e3f01ef81768e5e9
unknown <kassabksab@gmail.com>
2024-05-24 11:51:48 +0300

    if user login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/Screen/Sign_up_Screen.dart';
+import 'package:instagram/Screen/bottonBar.dart';
+import 'package:instagram/Screen/login_Screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,8 +19,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
-      home: Sign_up(),
-    );
+        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error'),
+              );
+            } else if (snapshot.data == null) {
+              return Login();
+            } else {
+              return BouttonBar();
+            }
+          },
+        ));
   }
 }
